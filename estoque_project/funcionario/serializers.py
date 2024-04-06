c53185a5ca530b9cd03082ec2127d943996f21a7
Luana Paes <luanafranca598@gmail.com>
2024-04-06 09:32:18 -0300

    Atualização na função cadastrar_funcionario
    - neste commit, consertei a função cadastrar_funcionario para que quando cadastrar o funcionário o id e o nome da empresa fiquem no seu registro.

--- estoque_project/funcionario/serializers.py
+++ estoque_project/funcionario/serializers.py
@@ -2,26 +2,31 @@ from rest_framework import serializers
 from funcionario.models import Funcionario
 from empresa.models import Empresa
 
-
-# class FuncionarioSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Funcionario
-#         fields = ('id', 'nome', 'email', 'tipo', 'empresa', 'nome_empresa')
-
 class FuncionarioSerializer(serializers.ModelSerializer):
     nome_empresa = serializers.CharField(max_length=255)
 
     class Meta:
         model = Funcionario
         fields = ('id', 'nome', 'email', 'tipo', 'nome_empresa')
-        read_only_fields = ('empresa',)  # apenas leitura do id empresa
+        read_only_fields = ('empresa',)  # torna o campo apenas leitura
+
+    def validate_nome_empresa(self, value):
+        try:
+            empresa = Empresa.objects.get(nome=value)
+            return empresa.nome
+        except Empresa.DoesNotExist:
+            raise serializers.ValidationError("A empresa não existe.")
 
     def create(self, validated_data):
         nome_empresa = validated_data.pop('nome_empresa')
 
-        # busca a empresa com base no nome fornecido na hora do cadastro
+        # busca a empresa com base no nome fornecido
         empresa = Empresa.objects.get(nome=nome_empresa)
 
-        # Define o objeto empresa no validated_data antes de criar o funcionário
+        # define o objeto empresa no validated_data antes de criar o funcionário
         validated_data['empresa'] = empresa
+
+        # defina o nome da empresa no campo nome_empresa - preenche na tabela auto
+        validated_data['nome_empresa'] = nome_empresa
+
         return Funcionario.objects.create(**validated_data)
--- estoque_project/funcionario/views.py
+++ estoque_project/funcionario/views.py
@@ -15,29 +15,6 @@ class FuncionarioViewSet(viewsets.ModelViewSet):
     serializer_class = FuncionarioSerializer
 
 
-# views.py
-
-# @api_view(['POST'])
-# def cadastrar_funcionario(request):
-#     if request.method == 'POST':
-#         serializer = FuncionarioSerializer(data=request.data)
-#         if serializer.is_valid():
-#             nome = serializer.validated_data.get('nome')
-#             email = serializer.validated_data.get('email')
-#             tipo = serializer.validated_data.get('tipo')
-#             empresa = serializer.validated_data.get('empresa')
-#             nome_empresa = serializer.validated_data.get('nome_empresa')
-
-#             funcionario = Funcionario.objects.create(
-#                 nome=nome, email=email, tipo=tipo, empresa=empresa, nome_empresa=nome_empresa)
-#             if funcionario:
-#                 return Response({'mensagem': 'Funcionário cadastrado com sucesso.'}, status=status.HTTP_201_CREATED)
-#             else:
-#                 return Response({'mensagem': 'Falha ao cadastrar funcionário. Esta empresa não existe no banco.'}, status=status.HTTP_400_BAD_REQUEST)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# -----------------------------------------------------------------------------------------
-
 @api_view(['POST'])
 def cadastrar_funcionario(request):
     if request.method == 'POST':
@@ -47,7 +24,7 @@ def cadastrar_funcionario(request):
 
             # verifica se o nome da empresa existe
             try:
-                empresa = Empresa.objects.get(nome=nome_empresa)
+                empresa, _ = Empresa.objects.get_or_create(nome=nome_empresa)
             except Empresa.DoesNotExist:
                 return Response({'mensagem': 'A empresa não existe.'}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -55,7 +32,6 @@ def cadastrar_funcionario(request):
             funcionario = serializer.save()
 
             if funcionario:
-                nome_empresa = request.data.get('nome_empresa')
                 return Response({'mensagem': 'Funcionário cadastrado com sucesso.'}, status=status.HTTP_201_CREATED)
             else:
                 return Response({'mensagem': 'Falha ao cadastrar funcionário.'}, status=status.HTTP_400_BAD_REQUEST)
