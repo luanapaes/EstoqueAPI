e17e45500641e65146115d8529bf06061a9db31b
Luana Paes <luanafranca598@gmail.com>
2024-04-07 23:10:11 -0300

    Mudança na models e na função de cadastro
    - caso o tipo cadastrado seja ADM o usuário é cadastrado como super user, caso ele seja cadastrado como FUNC ele é cadastrado como usuário comum;
    - adicionei o atributo senha - criptografadas;
    - antes de cadastrar verifica se a empresa existe, se o email e o nome já estão cadastrados para não permitir duplicidade, e uma função não permite que os usernames sejam iguais para resolver um erro que eu estava enfrentando.

--- estoque_project/funcionario/models.py
+++ estoque_project/funcionario/models.py
@@ -1,12 +1,45 @@
-from django.db import models
+from django.contrib.auth.hashers import make_password
 from empresa.models import Empresa
+
+from django.contrib.auth.models import User
+from django.db import models
+
+from django.contrib.auth.hashers import make_password
+from django.db.models.signals import post_save
 
 
 class Funcionario(models.Model):
     id = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=255)
+    nome = models.CharField(max_length=255, unique=True)
     email = models.EmailField()
+    senha = models.CharField(max_length=20)
     tipo = models.CharField(max_length=10, choices=[
         ('ADM', 'Administrador'), ('FUNC', 'Funcionário Comum')])
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
     nome_empresa = models.CharField(max_length=255, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.senha = make_password(self.senha)
+        super().save(*args, **kwargs)
+
+# se for tipo ADM criar super, se tipo FUNC tipo comum
+# FUNC = funcionário
+def create_user_from_funcionario(sender, instance, created, **kwargs):
+    if created:
+        if instance.tipo == "ADM":
+            user = User.objects.create_superuser(
+                username=instance.nome,
+                email=instance.email,
+                password=instance.senha,
+            )
+        elif instance.tipo == "FUNC":
+            user = User.objects.create_user(
+                username=instance.nome,
+                email=instance.email,
+                password=instance.senha,
+            )
+
+
+post_save.connect(create_user_from_funcionario, sender=Funcionario)
+
+
--- estoque_project/funcionario/views.py
+++ estoque_project/funcionario/views.py
@@ -2,9 +2,9 @@ from .serializers import FuncionarioSerializer
 from .models import Funcionario
 from empresa.models import Empresa
 from rest_framework.response import Response
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view
 from rest_framework import status
-from django.shortcuts import render
 from rest_framework import viewsets
 from funcionario.models import Funcionario
 from funcionario.serializers import FuncionarioSerializer
@@ -14,25 +14,67 @@ class FuncionarioViewSet(viewsets.ModelViewSet):
     queryset = Funcionario.objects.all()
     serializer_class = FuncionarioSerializer
 
+# ---------------------------------------------------------------------------------------------
+
+def generate_unique_username(nome):
+    # Gera um nome de usuário único a partir do nome do funcionário.
+
+    username = nome.lower().replace(' ', '_')
+    i = 1
+    while User.objects.filter(username=username).exists():
+        username = f'{username}_{i}'
+        i += 1
+    return username
+
+# ---------------------------------------------------------------------------------------------
 
 @api_view(['POST'])
 def cadastrar_funcionario(request):
-    if request.method == 'POST':
-        serializer = FuncionarioSerializer(data=request.data)
-        if serializer.is_valid():
-            nome_empresa = serializer.validated_data.get('nome_empresa')
+    serializer = FuncionarioSerializer(data=request.data)
+    if serializer.is_valid():
+        nome_empresa = serializer.validated_data.get('nome_empresa')
 
-            # verifica se o nome da empresa existe
-            try:
-                empresa, _ = Empresa.objects.get_or_create(nome=nome_empresa)
-            except Empresa.DoesNotExist:
-                return Response({'mensagem': 'A empresa não existe.'}, status=status.HTTP_400_BAD_REQUEST)
+        # verifica se o nome da empresa existe
+        try:
+            empresa = Empresa.objects.get(nome=nome_empresa)
+        except Empresa.DoesNotExist:
+            return Response({'mensagem': 'A empresa não existe.'}, status=status.HTTP_400_BAD_REQUEST)
 
-            # se a empresa existe, o serializer já associou a empresa ao funcionário
-            funcionario = serializer.save()
+        # se a empresa existe, associa-a ao funcionário para salvar só depois das validações
+        serializer.validated_data['empresa'] = empresa
 
-            if funcionario:
-                return Response({'mensagem': 'Funcionário cadastrado com sucesso.'}, status=status.HTTP_201_CREATED)
-            else:
-                return Response({'mensagem': 'Falha ao cadastrar funcionário.'}, status=status.HTTP_400_BAD_REQUEST)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # cria uma cópia dos dados do funcionário 
+        funcionario_data = serializer.validated_data.copy()
+
+        # verifica se o email já está cadastrado
+        if User.objects.filter(email=funcionario_data['email']).exists():
+            return Response({'mensagem': 'Já existe um usuário com este email.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # verifica o tipo de usuário
+        if funcionario_data['tipo'] not in ['ADM', 'FUNC']:
+            return Response({'mensagem': 'Tipo de usuário inválido.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # gera um nome de usuário único com a função
+        username = generate_unique_username(funcionario_data['nome'])
+
+        # cria o usuário
+        user = User.objects.create_user(
+            username=username,
+            email=funcionario_data['email'],
+            password=funcionario_data['senha'],
+        )
+
+        # salva o funcionário no banco
+        funcionario = serializer.save()
+        if funcionario:
+            # atribui o usuário ao funcionário
+            funcionario.user = user
+            funcionario.save()
+
+            return Response({'mensagem': 'Funcionário cadastrado com sucesso.'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'mensagem': 'Falha ao cadastrar funcionário.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# ---------------------------------------------------------------------------------------------
